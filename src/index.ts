64aba52993db4ea25bd52ff1a8cca6d33bb62cef
Omkar_Shirsekar <iamomkar0007@gmail.com>
2025-12-10 17:00:54 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { WebSocketServer , WebSocket } from 'ws'
+import { WebSocketServer, WebSocket } from "ws";
 
-let AllSockets:WebSocket[]=[];
+let AllSockets: WebSocket[] = [];
 
-const wss=new WebSocketServer({ port:8080 });
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection',(socket)=>{
-    AllSockets.push(socket);
+wss.on("connection", (socket) => {
+  AllSockets.push(socket);
 
-    console.log('connections has been established')
+  console.log("connections has been established");
 
-    socket.on("message",(message)=>{
-        for(let i=0;i<AllSockets.length;i++){
-            const s=AllSockets[i];
-            s?.send(message + " Coming from the server")
-        }
-    })
-})
+  socket.on("message", (message) => {
+    for (let i = 0; i < AllSockets.length; i++) {
+      const s = AllSockets[i];
+      s?.send(message + " Coming from the server");
+    }
+  });
+});
